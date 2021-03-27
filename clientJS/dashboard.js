365242958beb635861d69fd66318cee709070c59
Aayush Wadhwani <aayushwadhwani99@gmail.com>
2021-03-28 00:07:13 +0530

    :sparkles: display cardsof class

--- clientJS/dashboard.js
+++ clientJS/dashboard.js
@@ -13,17 +13,52 @@ const showClass = (className) => {
   localStorage.setItem("currentClassName", className);
   window.location.href = "/class";
 };
+
 const getData = () => {
   userDetails = document.getElementById("userDetails").value;
   //   if (localStorage.getItem("userDetails").length < 10)
   localStorage.setItem("userDetails", userDetails);
   userDetails = JSON.parse(userDetails);
-  let btn = "";
+  let html = "";
+  let h2 =""; /*
+
+  */
+  let rowsCount = 1;
+  let colCount = 1;
+  let semCount = 0; //2-
+  let temp = semCount;
   userDetails.sorted.forEach((c) => {
-    btn += `<button onclick='showClass("${c.className}")'>${c.className}</button> <br>`;
+    if(semCount !== c.sem)
+    {
+      h2 += `
+      <h2 class="row-start-${rowsCount/*1*/} col-start-1 col-span-full" > Semester - ${c.sem} </h2>
+      `;
+      semCount = c.sem; //2-3-4
+      colCount = 1; //1-1-1
+      rowsCount++; //2-3-4
+      console.log(rowsCount)
+    }
+    html = `
+      <div class="card h-28 row-start-${rowsCount} col-start-${colCount} col-span-4" onclick='showClass("${c.className}")'>
+        <span>${c.className}</span>
+      </div>
+    `;
+    h2+=html;
+    colCount+=4;
+    if(colCount > 12 ){
+      colCount = 1;
+      rowsCount++;
+    }
+    if(temp !== semCount)
+    {
+      rowsCount++;
+      temp = semCount;
+    }
   });
-  document.getElementById("allClasses").innerHTML = btn;
+  // document.getElementsByClassName("h2").innerHTML = c.sem;
+  document.getElementById("allClasses").innerHTML = h2
 };
+
 const addClass = () => {
   let userDetails = {};
   if (localStorage.getItem("userDetails").length > 10) {
